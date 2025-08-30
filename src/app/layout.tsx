54f6eb03cb43ba8680940c79ce3f8fb114a61374
Aayush Chaudhary <skrrtaltra@gmail.com>
2025-08-30 23:13:34 +0000

    use space grotesk as font and according to the heading or requirement ma

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -52,7 +52,7 @@ export default function RootLayout({
           crossOrigin="anonymous"
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;900&family=Space+Grotesk:wght@500;600;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300..700&display=swap"
           rel="stylesheet"
         />
       </head>
